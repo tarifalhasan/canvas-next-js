c13e5e5815a77362249913b0f5b3ce700f30a10c
tarifalhasan <tarialhasan999@gmail.com>
2023-07-20 22:24:33 +0600

    done

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,9 @@
-'use client';
-
-import { Button } from '@material-tailwind/react';
 import Link from 'next/link';
 
 export default function Home() {
   return (
-    <Button>
+    <>
       <Link href={'products'}>Go to Product Page</Link>
-    </Button>
+    </>
   );
 }
